2ed4be8336af2dd5c1c941cb860c744ad58e769e
kostasdrakonakis <kostasdrakonakis13@gmail.com>
2020-12-10 15:13:26 +0100

    Created custom LogAssert Subject to properly unit test logging

---
+++ app/src/test/java/com/kostasdrakonakis/notes/logger/LogAssertSubject.kt
@@ -0,0 +1,158 @@
+package com.kostasdrakonakis.notes.logger
+
+import android.util.Log
+import com.google.common.truth.FailureMetadata
+import com.google.common.truth.Subject
+import com.google.common.truth.Subject.Factory
+import com.google.common.truth.Truth.assertAbout
+import com.google.common.truth.Truth.assertThat
+import com.kostasdrakonakis.notes.BuildConfig
+import org.robolectric.shadows.ShadowLog
+import kotlin.reflect.KClass
+
+class LogAssertSubject(
+    metadata: FailureMetadata?,
+    private val actual: NoteLogger?,
+    private val items: List<ShadowLog.LogItem>
+) : Subject(metadata, actual) {
+
+    private var index: Int = 1
+
+    fun hasVerboseMessage(tag: String, message: String): LogAssertSubject {
+        return hasMessage(Log.VERBOSE, tag, message)
+    }
+
+    fun hasVerboseExceptionMessage(
+        tag: String,
+        message: String,
+        exception: Throwable
+    ): LogAssertSubject {
+        return hasExceptionMessage(Log.VERBOSE, tag, message, exception)
+    }
+
+    fun hasDebugMessage(tag: String, message: String): LogAssertSubject {
+        return hasMessage(Log.DEBUG, tag, message)
+    }
+
+    fun hasDebugExceptionMessage(
+        tag: String,
+        message: String,
+        exception: Throwable
+    ): LogAssertSubject {
+        return hasExceptionMessage(Log.DEBUG, tag, message, exception)
+    }
+
+    fun hasInfoMessage(tag: String, message: String): LogAssertSubject {
+        return hasMessage(Log.INFO, tag, message)
+    }
+
+    fun hasInfoExceptionMessage(
+        tag: String,
+        message: String,
+        exception: Throwable
+    ): LogAssertSubject {
+        return hasExceptionMessage(Log.INFO, tag, message, exception)
+    }
+
+    fun hasWarnMessage(tag: String, message: String): LogAssertSubject {
+        return hasMessage(Log.WARN, tag, message)
+    }
+
+    fun hasWarnExceptionMessage(
+        tag: String,
+        message: String,
+        exception: Throwable
+    ): LogAssertSubject {
+        return hasExceptionMessage(Log.WARN, tag, message, exception)
+    }
+
+    fun hasErrorMessage(tag: String, message: String): LogAssertSubject {
+        return hasMessage(Log.ERROR, tag, message)
+    }
+
+    fun hasErrorExceptionMessage(
+        tag: String,
+        message: String,
+        exception: Throwable
+    ): LogAssertSubject {
+        return hasExceptionMessage(Log.ERROR, tag, message, exception)
+    }
+
+    fun hasAssertMessage(tag: String, message: String): LogAssertSubject {
+        return hasMessage(Log.ASSERT, tag, message)
+    }
+
+    fun hasAssertExceptionMessage(
+        tag: String,
+        message: String,
+        exception: Throwable
+    ): LogAssertSubject {
+        return hasExceptionMessage(Log.ASSERT, tag, message, exception)
+    }
+
+    fun isInstance(clazz: KClass<NoteLogger>) {
+        assertThat(actual?.javaClass?.simpleName).isEqualTo(clazz.java.simpleName)
+    }
+
+    fun hasNoMoreMessages() {
+        assertThat(items).hasSize(++index)
+    }
+
+    fun isDebugEnabled() {
+        assertThat(actual?.isDebugEnabled).isEqualTo(BuildConfig.DEBUG)
+    }
+
+    fun isErrorEnabled() {
+        assertThat(actual?.isErrorEnabled).isEqualTo(BuildConfig.DEBUG)
+    }
+
+    fun isTraceEnabled() {
+        assertThat(actual?.isTraceEnabled).isEqualTo(BuildConfig.DEBUG)
+    }
+
+    fun isInfoEnabled() {
+        assertThat(actual?.isInfoEnabled).isEqualTo(BuildConfig.DEBUG)
+    }
+
+    fun isWarnEnabled() {
+        assertThat(actual?.isWarnEnabled).isEqualTo(BuildConfig.DEBUG)
+    }
+
+    fun hasNameEqualTo(name: String) {
+        assertThat(actual?.name).isEqualTo(name)
+    }
+
+    private fun hasMessage(priority: Int, tag: String, message: String): LogAssertSubject {
+        val item = items[index]
+        assertThat(item.type).isEqualTo(priority)
+        assertThat(item.tag).isEqualTo(tag)
+        assertThat(item.msg).isEqualTo(message)
+        return this
+    }
+
+    private fun hasExceptionMessage(
+        priority: Int,
+        tag: String,
+        message: String,
+        exception: Throwable
+    ): LogAssertSubject {
+        val item = items[index]
+        assertThat(item.type).isEqualTo(priority)
+        assertThat(item.tag).isEqualTo(tag)
+        assertThat(item.msg).startsWith(message)
+        assertThat(exception).isInstanceOf(Throwable::class.java)
+        return this
+    }
+
+    companion object {
+        fun assertThat(actual: NoteLogger?): LogAssertSubject {
+            return assertAbout(logFactory()).that(actual)
+        }
+
+        private fun logFactory(): Factory<LogAssertSubject, NoteLogger> {
+            return Factory<LogAssertSubject, NoteLogger> { metadata, actual ->
+                LogAssertSubject(metadata, actual, ShadowLog.getLogs())
+            }
+        }
+    }
+}
--- app/src/test/java/com/kostasdrakonakis/notes/logger/NoteLoggerTest.kt
+++ app/src/test/java/com/kostasdrakonakis/notes/logger/NoteLoggerTest.kt
@@ -1,9 +1,7 @@
 package com.kostasdrakonakis.notes.logger
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.common.truth.Truth.assertThat
-import com.kostasdrakonakis.notes.BuildConfig
-import com.kostasdrakonakis.notes.logger.LogAssert.Companion.assertLog
+import com.kostasdrakonakis.notes.logger.LogAssertSubject.Companion.assertThat
 import com.kostasdrakonakis.notes.util.LogUtil
 import org.junit.After
 import org.junit.Before
@@ -41,114 +39,114 @@ class NoteLoggerTest {
 
     @Test
     fun verifyName() {
-        assertThat(logger.name).isEqualTo(LOGGER_NAME)
+        assertThat(logger).hasNameEqualTo(LOGGER_NAME)
     }
 
     @Test
     fun isDebugEnabled() {
-        assertThat(logger.isDebugEnabled).isEqualTo(BuildConfig.DEBUG)
+        assertThat(logger).isDebugEnabled()
     }
 
     @Test
     fun isTraceEnabled() {
-        assertThat(logger.isTraceEnabled).isEqualTo(BuildConfig.DEBUG)
+        assertThat(logger).isTraceEnabled()
     }
 
     @Test
     fun isInfoEnabled() {
-        assertThat(logger.isInfoEnabled).isEqualTo(BuildConfig.DEBUG)
+        assertThat(logger).isInfoEnabled()
     }
 
     @Test
     fun isWarnEnabled() {
-        assertThat(logger.isWarnEnabled).isEqualTo(BuildConfig.DEBUG)
+        assertThat(logger).isWarnEnabled()
     }
 
     @Test
     fun isErrorEnabled() {
-        assertThat(logger.isErrorEnabled).isEqualTo(BuildConfig.DEBUG)
+        assertThat(logger).isErrorEnabled()
     }
 
     @Test
     fun verifyLogger() {
-        assertThat(LogUtil.logger<NoteLoggerTest>()).isInstanceOf(logger.javaClass)
+        assertThat(logger).isInstance(NoteLogger::class)
     }
 
     @Test
     fun verifyDebugMessage() {
         logger.debug(DEBUG_MESSAGE)
-        assertLog().hasDebugMessage(TAG, DEBUG_MESSAGE).hasNoMoreMessages()
+        assertThat(logger).hasDebugMessage(TAG, DEBUG_MESSAGE).hasNoMoreMessages()
     }
 
     @Test
     fun verifyDebugExceptionMessage() {
         val exception = Throwable(DEBUG_MESSAGE)
         logger.debug(DEBUG_MESSAGE, exception as Throwable?)
-        assertLog().hasDebugExceptionMessage(TAG, DEBUG_MESSAGE, exception).hasNoMoreMessages()
+        assertThat(logger).hasDebugExceptionMessage(TAG, DEBUG_MESSAGE, exception).hasNoMoreMessages()
     }
 
     @Test
     fun verifyWarningMessage() {
         logger.warn(WARN_MESSAGE)
-        assertLog().hasWarnMessage(TAG, WARN_MESSAGE).hasNoMoreMessages()
+        assertThat(logger).hasWarnMessage(TAG, WARN_MESSAGE).hasNoMoreMessages()
     }
 
     @Test
     fun verifyWarningExceptionMessage() {
         val exception = Throwable(WARN_MESSAGE)
         logger.warn(WARN_MESSAGE, exception as Throwable?)
-        assertLog().hasWarnExceptionMessage(TAG, WARN_MESSAGE, exception).hasNoMoreMessages()
+        assertThat(logger).hasWarnExceptionMessage(TAG, WARN_MESSAGE, exception).hasNoMoreMessages()
     }
 
     @Test
     fun verifyInfoMessage() {
         logger.info(INFO_MESSAGE)
-        assertLog().hasInfoMessage(TAG, INFO_MESSAGE).hasNoMoreMessages()
+        assertThat(logger).hasInfoMessage(TAG, INFO_MESSAGE).hasNoMoreMessages()
     }
 
     @Test
     fun verifyInfoExceptionMessage() {
         val exception = Throwable(INFO_MESSAGE)
         logger.info(INFO_MESSAGE, exception as Throwable?)
-        assertLog().hasInfoExceptionMessage(TAG, INFO_MESSAGE, exception).hasNoMoreMessages()
+        assertThat(logger).hasInfoExceptionMessage(TAG, INFO_MESSAGE, exception).hasNoMoreMessages()
     }
 
     @Test
     fun verifyVerboseMessage() {
         logger.trace(TRACE_MESSAGE)
-        assertLog().hasVerboseMessage(TAG, TRACE_MESSAGE).hasNoMoreMessages()
+        assertThat(logger).hasVerboseMessage(TAG, TRACE_MESSAGE).hasNoMoreMessages()
     }
 
     @Test
     fun verifyVerboseExceptionMessage() {
         val exception = Throwable(TRACE_MESSAGE)
         logger.trace(TRACE_MESSAGE, exception as Throwable?)
-        assertLog().hasVerboseExceptionMessage(TAG, TRACE_MESSAGE, exception).hasNoMoreMessages()
+        assertThat(logger).hasVerboseExceptionMessage(TAG, TRACE_MESSAGE, exception).hasNoMoreMessages()
     }
 
     @Test
     fun verifyErrorMessage() {
         logger.error(ERROR_MESSAGE)
-        assertLog().hasErrorMessage(TAG, ERROR_MESSAGE).hasNoMoreMessages()
+        assertThat(logger).hasErrorMessage(TAG, ERROR_MESSAGE).hasNoMoreMessages()
     }
 
     @Test
     fun verifyErrorExceptionMessage() {
         val exception = Throwable(ERROR_MESSAGE)
         logger.error(ERROR_MESSAGE, exception as Throwable?)
-        assertLog().hasErrorExceptionMessage(TAG, ERROR_MESSAGE, exception).hasNoMoreMessages()
+        assertThat(logger).hasErrorExceptionMessage(TAG, ERROR_MESSAGE, exception).hasNoMoreMessages()
     }
 
     @Test
     fun verifyAssertMessage() {
         logger.assert(ASSERT_MESSAGE)
-        assertLog().hasAssertMessage(TAG, ASSERT_MESSAGE).hasNoMoreMessages()
+        assertThat(logger).hasAssertMessage(TAG, ASSERT_MESSAGE).hasNoMoreMessages()
     }
 
     @Test
     fun verifyAssertExceptionMessage() {
         val exception = Throwable(ASSERT_MESSAGE)
         logger.assert(ASSERT_MESSAGE, exception as Throwable?)
-        assertLog().hasAssertExceptionMessage(TAG, ASSERT_MESSAGE, exception).hasNoMoreMessages()
+        assertThat(logger).hasAssertExceptionMessage(TAG, ASSERT_MESSAGE, exception).hasNoMoreMessages()
     }
 }
